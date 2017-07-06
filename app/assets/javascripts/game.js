a21d5f3b27b2be6aa8f603d653693703cd517f21
Lucas Kuhn <email": null>
2017-07-05 17:57:11 -0700

    adds build button after selecting building

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,5 +1,3 @@
-
-
 var apt, sprite;
 
 
@@ -28,7 +26,12 @@ BasicGame.Boot.prototype =
         game.load.image('cube', 'images/cube.png');
         game.load.image('apt', 'city-builder/aptcomplex_SE.png');
         game.load.image('burgerSW', 'city-builder/burger_SW.png');
+        game.load.image('red-button', "redbutton.png")
+        game.load.image('right-button', "right.png")
+
+
         game.load.image('background', "background.jpg")
+
 
         // Used to show the FPS
         game.time.advancedTiming = true;
@@ -45,28 +48,30 @@ BasicGame.Boot.prototype =
         // Adding Background
         game.add.tileSprite(0, 0, width, height, 'background');
 
-        // --------------- CLICKABLE BUILDINGS
 
-        // BURGER
-        clickableSprite = game.add.sprite(200, 100, 'burgerSW');
-        clickableSprite.fixedToCamera = true;
-        clickableSprite.inputEnabled = true;
-        clickableSprite.alpha = 0.8
-        clickableSprite.events.onInputDown.add(addBurger, this);
+        // TESTING BUTTONS
+
+        // RED BUTTON
+        cE = game.add.sprite(400, 0, 'red-button');
+        cE.fixedToCamera = true;
+        cE.inputEnabled = true;
+        cE.events.onInputDown.add(addBuilding, this);
+
+        // RIGHT BUTTON
+        cE = game.add.sprite(210, 60, 'right-button');
+        cE.fixedToCamera = true;
+        cE.inputEnabled = true;
+        cE.events.onInputDown.add(onDown, this);
+        // cE.events.onInputOver.add(onDown, this);
+        // cE.events.onInputUp.add(onUp, this);
+        // cE.events.onInputOut.add(onUp, this);
+
+        // --------------- CLICKABLE BUILDING BUTTONS
 
         // APARTMENT
-        clickableSprite = game.add.sprite(20, 20, 'apt');
-        clickableSprite.fixedToCamera = true;
-        clickableSprite.inputEnabled = true;
-        clickableSprite.alpha = 0.8
-        clickableSprite.events.onInputDown.add(addApt, this);
-
-        // LVL 1 BUILDING
-        clickableSprite = game.add.sprite(400, 20, 'lvl1');
-        clickableSprite.fixedToCamera = true;
-        clickableSprite.inputEnabled = true;
-        clickableSprite.alpha = 0.8
-        clickableSprite.events.onInputDown.add(addLvl1, this);
+        buildingButton = game.add.sprite(20, 20, 'apt');
+        buildingButton.fixedToCamera = true;
+        buildingButton.alpha = 0.8
 
 
         // Create a group for our tiles.
@@ -79,9 +84,6 @@ BasicGame.Boot.prototype =
 
         // Provide a 3D position for the cursor
         cursorPos = new Phaser.Plugin.Isometric.Point3();
-
-
-
     },
     update: function () {
         // Update the cursor position.
@@ -93,28 +95,36 @@ BasicGame.Boot.prototype =
 
         // Loop through all tiles and test to see if the 3D position from above intersects with the automatically generated IsoSprite tile bounds.
         isoGroup.forEach(function (tile) {
-            var inBounds = tile.isoBounds.containsXY(cursorPos.x, cursorPos.y);
+            //  If a building should be added, add the building
             if (tile.busy && !tile.buildingAdded){
                 game.add.isoSprite(tile.isoX + tile.buildingX,tile.isoY + tile.buildingY, tile.buildingZ, tile.buildingName, 0, buildingGroup);
                 tile.buildingAdded = true;
             }
 
-            // If it does, do a little animation and tint change.
+            //Check if the cursor is over a tile
+            var inBounds = tile.isoBounds.containsXY(cursorPos.x, cursorPos.y);
+
+            // WHEN CLICKING: Select the tile and mark it as ready to build
             if (game.input.activePointer.isDown && inBounds && !tile.busy){
-                console.log("CLICKED!")
-                selectedTile = tile
+                    // Un-mark any other tiles
+                isoGroup.forEach(function (tile) {tile.ready = false})
+                    // Mark it as ready and paint it green
                 tile.ready = true
                 tile.tint = 0x00FF00;
+                    // Do the Ease Out animation so it doesn't stay up
                 game.add.tween(tile).to({ isoZ: 0 }, 200, Phaser.Easing.Quadratic.InOut, true);
+                    // Reference this tile for the building methods
+                selectedTile = tile
             }
+
+            // WHEN HOVERING: do a little animation and tint change.
             else if (!tile.selected && inBounds) {
-                console.log("PAINTING BLUE");
                 tile.selected = true;
                 tile.tint = 0x86bfda;
                 game.add.tween(tile).to({ isoZ: 4 }, 200, Phaser.Easing.Quadratic.InOut, true);
             }
 
-            // If not, revert back to how it was.
+            // If the mouse is not over the tile, revert back to how it was.
             else if (tile.selected && !inBounds && !tile.ready) {
                 tile.selected = false;
                 tile.tint = 0xffffff;
@@ -124,12 +134,14 @@ BasicGame.Boot.prototype =
 
 
     },
+
+    // DISPLAY TEXT AND FPS
     render: function () {
-        game.debug.text("Move your mouse around!", 2, 36, "#ffffff");
+        game.debug.text("ADD A BUILDING!", 2, 36, "#ffffff");
         game.debug.text(game.time.fps || '--', 2, 14, "#a7aebe");
 
-
     },
+    // ADD THE MAP TILES
     spawnTiles: function () {
         var tile;
         for (var xx = 0; xx < 512; xx += 76) {
@@ -211,7 +223,50 @@ function renderProperly (){
     // Set the building added as false so the building are re-rendered in the right order
     isoGroup.forEach(function (tile) {tile.buildingAdded = false});
 };
+
+
+var i = 0
+function onDown(sprite, pointer) {
+    switch (i) {
+      case 1:
+        buildingButton.loadTexture('tree1', 0);
+        break;
+        //Statements executed when the result of expression matches value2
+      case 2:
+        buildingButton.loadTexture('apt', 0);
+        break;
+      //Statements executed when the result of expression matches value2
+      case 3:
+        buildingButton.loadTexture('lvl1', 0);
+        break;
+        //Statements executed when the result of expression matches value2
+      case 4:
+        buildingButton.loadTexture('burgerSW', 0);
+        break;
+        //Statements executed when the result of expression matches value2
+      default:
+        i = 0;
+        //Statements executed when none of the values match the value of the expression
+    }
+    i += 1
+}
+function addBuilding(sprite, pointer) {
+    switch (buildingButton.key) {
+      case 'apt':
+        addApt();
+        break;
+      case 'lvl1':
+        addLvl1();
+        break;
+      case 'burgerSW':
+        addBurger();
+        break;
+    }
+}
+
+
 // ---------------------------------
+
 
 // START THE GAME
 game.state.add('Boot', BasicGame.Boot);
